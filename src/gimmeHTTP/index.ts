ce1498e93ce3f074ff56b4355ba39b12596df7b9
brianvoe <brian@webiswhatido.com>
2024-11-12 20:40:39 -0600

    index - added all here

--- src/gimmeHTTP/index.ts
+++ src/gimmeHTTP/index.ts
@@ -7,6 +7,46 @@ import { IsJsonRequest } from './utils/utils'
 import type { Target } from './utils/registry'
 import type { Method, Request, Config, Http } from './utils/generate'
 
+// Targets
+import CLibCurl from './languages/c.libcurl'
+import CSharpHttp from './languages/csharp.http'
+import CSharpRest from './languages/csharp.restsharp'
+import Curl from './languages/curl'
+import Go from './languages/go'
+import JSFetch from './languages/js.fetch'
+import JSAxios from './languages/js.axios'
+import JSJquery from './languages/js.jquery'
+import Node from './languages/node.http'
+import NodeFetch from './languages/node.fetch'
+import PHPCurl from './languages/php.curl'
+import PHPGuzzle from './languages/php.guzzle'
+import PythonHttp from './languages/python.http'
+import PythonRequests from './languages/python.requests'
+import Ruby from './languages/ruby.nethttp'
+import RubyFaraday from './languages/ruby.faraday'
+import RustReqwest from './languages/rust.reqwest'
+import SwiftNsurlsession from './languages/swift.nsurlsession'
+
+// Register all languages
+Register(CLibCurl)
+Register(CSharpHttp)
+Register(CSharpRest)
+Register(Curl)
+Register(Go)
+Register(JSFetch)
+Register(JSAxios)
+Register(JSJquery)
+Register(Node)
+Register(NodeFetch)
+Register(PHPCurl)
+Register(PHPGuzzle)
+Register(PythonHttp)
+Register(PythonRequests)
+Register(Ruby)
+Register(RubyFaraday)
+Register(RustReqwest)
+Register(SwiftNsurlsession)
+
 export {
   // Registry
   Target,
