d02135b22d78b31f96b3ecf4147c7daea1fc03e1
brianvoe <brian@webiswhatido.com>
2024-11-29 09:56:07 -0600

    main - simplify main by removing highlightjs

--- src/docs/main.ts
+++ src/docs/main.ts
@@ -1,23 +1,10 @@
+import './assets/scss/index.scss'
+
 import { createApp } from 'vue'
 import router from './router'
 
 import App from './app.vue'
-import Prism from 'prismjs'
-import Normalizer from 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace'
-import 'prismjs/plugins/toolbar/prism-toolbar'
-
-new Normalizer({
-  'remove-trailing': true,
-  'remove-indent': true,
-  'left-trim': true,
-  'right-trim': true
-})
 
 const app = createApp(App)
 app.use(router)
-app.mixin({
-  updated() {
-    Prism.highlightAll()
-  }
-})
 app.mount('#app')
