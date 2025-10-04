5ea52997b3a43d8127e91ec3df939f098f6c1d4d
brianvoe <brian@webiswhatido.com>
2025-10-03 21:14:14 -0500

    output - dist update

---
+++ dist/vue/gimmehttp.vue.d.ts
@@ -0,0 +1,83 @@
+import { PropType } from 'vue';
+import { Config, Http } from '../utils/generate';
+declare const _default: import('vue').DefineComponent<import('vue').ExtractPropTypes<{
+    http: {
+        type: PropType<Http>;
+        required: true;
+    };
+    language: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    client: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    config: {
+        type: PropType<Config>;
+        required: false;
+    };
+    theme: {
+        type: PropType<"light" | "dark">;
+        required: false;
+    };
+}>, {}, {
+    highlighter: any;
+    clientsList: import('../index').Client[];
+    showCopied: boolean;
+    openModal: boolean;
+    openModalContent: boolean;
+    codeStr: string;
+    output: string;
+    themeMode: "light" | "dark";
+    internalLanguage: string;
+    internalClient: string;
+    checkInterval: number | null;
+}, {
+    languages(): string[];
+    clients(): string[];
+    shikiTheme(): string;
+}, {
+    logoHref(name: string): string;
+    setLanguage(lang: string | null): void;
+    setClient(client: string | null): void;
+    code(): void;
+    clickCopy(): void;
+    toggleModal(): void;
+    clickModalBg(event: MouseEvent): void;
+    clickModalLang(lang: string): void;
+    clickModalClient(client: string): void;
+    checkLocalStorage(): void;
+}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, ("update:language" | "update:client")[], "update:language" | "update:client", import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{
+    http: {
+        type: PropType<Http>;
+        required: true;
+    };
+    language: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    client: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    config: {
+        type: PropType<Config>;
+        required: false;
+    };
+    theme: {
+        type: PropType<"light" | "dark">;
+        required: false;
+    };
+}>> & Readonly<{
+    "onUpdate:language"?: (...args: any[]) => any;
+    "onUpdate:client"?: (...args: any[]) => any;
+}>, {
+    language: string;
+    client: string;
+}, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {}, any>;
+export default _default;
---
+++ dist/vue/index.d.ts
@@ -0,0 +1,7 @@
+import { App } from 'vue';
+import { default as GimmeHttp } from './gimmehttp.vue';
+export { GimmeHttp };
+declare const _default: {
+    install(app: App): void;
+};
+export default _default;
