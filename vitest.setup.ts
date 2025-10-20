e5ee40a12b38245b04932b4acd294d310ba5fc40
brianvoe <brian@webiswhatido.com>
2025-10-19 20:37:31 -0500

    vitest - added setup for mediaMatches

---
+++ vitest.setup.ts
@@ -0,0 +1,15 @@
+// Setup file for Vitest with jsdom
+// Mock matchMedia for jsdom environment
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => true
+  })
+})
