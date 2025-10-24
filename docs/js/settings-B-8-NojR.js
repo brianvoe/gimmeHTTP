4d6e218a746d83522d57b71689b127b59700424e
brianvoe <brian@webiswhatido.com>
2025-10-24 18:38:20 -0500

    output - dist/docs update

--- docs/js/settings-B-8-NojR.js
+++ docs/js/settings-B-8-NojR.js
@@ -1,4 +1,4 @@
-import{S as s}from"./shiki_style.js";import{d as a,_ as o,r as i,c as r,o as l,a as e,b as d,e as p,i as c}from"./index.js";import"./github-light.js";const g=a({components:{ShikiStyle:s},name:"Settings"}),m={class:"section settings"};function u(f,n,h,y,S,_){const t=i("ShikiStyle");return l(),r("div",m,[n[1]||(n[1]=e("h2",null,"Settings",-1)),n[2]||(n[2]=e("div",{class:"alert info"},[e("strong",null,"Info:"),p(" The only required fields are language, http.method and http.url")],-1)),d(t,{language:"javascript"},{default:c(()=>[...n[0]||(n[0]=[e("pre",null,`        const settings = {
+import{d as s,S as a,_ as o,e as i,g as e,j as r,m as l,A as d,l as c,o as p}from"./shiki-BVECRVGe.js";const g=s({components:{ShikiStyle:a},name:"Settings"}),m={class:"section settings"};function u(f,n,h,y,S,_){const t=c("ShikiStyle");return p(),i("div",m,[n[1]||(n[1]=e("h2",null,"Settings",-1)),n[2]||(n[2]=e("div",{class:"alert info"},[e("strong",null,"Info:"),l(" The only required fields are language, http.method and http.url")],-1)),r(t,{language:"javascript"},{default:d(()=>[...n[0]||(n[0]=[e("pre",null,`        const settings = {
           // Required
           language: 'javascript', // Programming Language
           client: 'axios',        // Client - default set by language
@@ -37,4 +37,4 @@ import{S as s}from"./shiki_style.js";import{d as a,_ as o,r as i,c as r,o as l,a
             }
           }
         }
-      `,-1)])]),_:1})])}const C=o(g,[["render",u]]);export{C as default};
+      `,-1)])]),_:1})])}const x=o(g,[["render",u]]);export{x as default};
